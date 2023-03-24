8d011d0e1e33a7d5950c93eaa75ed82e1a4f8b8a
BeepBoopBit <w.cbrenzaguirre@gmail.com>
2023-03-24 12:23:39 +0800

    Added PalindromeNumber.cpp

---
+++ Easy/PalindromeNumber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int tempNumber = 0, tempX = x;
+
+        while(tempX > 0){
+            tempNumber = tempNumber*10 + tempX%10;
+            tempX/=10;
+        }
+        return (tempNumber) == x;
+    }
+};
+
+int main(){
+    Solution sol;
+    std::cout << sol.isPalindrome(123) << std::endl;
+
+    return 0;
+}
+
+
+
+
+// 123 % 10 = 3 -> 300
+// 123 / 10 = 12
--- Easy/TwoSum.cpp
+++ Easy/TwoSum.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <map>
 #include <vector>
 
 using namespace std;
@@ -5,16 +7,28 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        auto vectSize = nums.size();
-        for(int i = 0; i < vectSize; ++i){
-            for(int j = 0; j < vectSize; ++j){
-                if(i == j){
-                    
-                }else if(nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+        std::map<int, int> myMap;
+        for(int i = 0; i < nums.size(); ++i){
+            myMap[nums[i]] = i;            
+        }
+        for(int i = 0; i < nums.size(); ++i){
+            int complement = target - nums[i];
+            auto x = myMap.find(complement);
+            if(x != myMap.end() && x->second != i){
+                return {i, x->second};
             }
         }
         return {0};
     }
 };
+
+
+int main(){
+    Solution s;
+    vector<int> myVect = {3,2,4};
+    vector<int> result = s.twoSum(myVect, 6);
+    for(int i : result){
+        cout << i << " ";
+    }
+    return 0;
+}
